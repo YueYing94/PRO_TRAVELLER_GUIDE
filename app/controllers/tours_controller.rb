94af31b1601565f2fd775171090607e9d46eb6eb
yue ying <sarahyingyue1994@gmail.com>
2023-06-17 21:27:18 +0200

    profile page structure done

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,4 +2,23 @@ class ToursController < ApplicationController
   def index
     @tours = Tour.all
   end
+
+  def create
+    @tour = Tour.new(tour_params)
+    @tour.user = current_user
+    @tour.save
+    redirect_to profile_path
+  end
+
+  def update
+    @tour = Tour.find(params[:id])
+    @tour.update(tour_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def tour_params
+    params.require(:tour).permit(:name, :date, :time, :description, :capacity, :duration, :start_point, :user_id, photos: [])
+  end
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,4 +1,5 @@
 class Tour < ApplicationRecord
   belongs_to :user
   validates :name, :date, :time, :description, :capacity, :duration, :start_point, presence: true
+  has_many_attached :photos
 end
