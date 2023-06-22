3096e0de5fb8afb0b8d421cbd2d28ce206919413
yue ying <sarahyingyue1994@gmail.com>
2023-06-22 19:01:04 +0200

    update booking model

--- db/migrate/20230622165330_create_bookings.rb
+++ db/migrate/20230622165330_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
       t.references :tour, null: false, foreign_key: true
       t.string :status
 
