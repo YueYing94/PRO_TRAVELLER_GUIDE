c0a395bc988336e88d67b64d5e01159ca670b2e0
max delaurey <mvxdelaurey@gmail.com>
2023-06-23 23:50:33 +0100

    refactor of booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,15 @@
 class BookingsController < ApplicationController
   def create
     @tour = Tour.find(params[:tour_id])
-    # authorize @tour
-    # if @tour.user == current_user
-    #   flash.alert = "Cannot book your own tour."
-    #   redirect_to tour_path(@tour)
-    # else
-      @booking = Booking.new
-      @booking.tour = @tour
-      @booking.user = current_user
-      @booking.status = "pending..."
-      # authorize @booking
-      if @booking.save
-        redirect_to tour_path(@tour)
-      else
-        render "tours/index", status: :unprocessable_entity
-      end
-
-    # end
+    @booking = Booking.new
+    @booking.tour = @tour
+    @booking.user = current_user
+    @booking.status = "pending..."
+    # authorize @booking
+    if @booking.save
+      redirect_to tour_path(@tour)
+    else
+      render "tours/index", status: :unprocessable_entity
+    end
   end
 end
