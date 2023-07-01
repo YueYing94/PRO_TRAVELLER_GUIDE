fd8ba207b21466c518962390d622302670e6b6c5
max delaurey <mvxdelaurey@gmail.com>
2023-07-01 15:03:55 +0100

    accurate booking capaticies in the modal & cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,19 +44,18 @@ file1 = URI.open("https://images.unsplash.com/photo-1568213816046-0ee1c42bd559?i
 file2 = URI.open("https://images.unsplash.com/photo-1575032748932-70116d68ed58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80")
 file3 = URI.open("https://images.unsplash.com/photo-1469533802599-76ee5be68f7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80")
 wine_tasting = Tour.new(name: "Wine-tasting", date: "30/06/2023", time: "16:00", description: "Bring your taste-buds to life with sips of vintages!", capacity: 4, duration: "45 minutes", start_point: "120 Av. Lucien Salentiny, 9080 Ettelbruck, Luxembourg")
-wine_tasting.photos.attach(io: file1, filename: "Wine.png", content_type: "image/png")
-wine_tasting.photos.attach(io: file2, filename: "Wine.png", content_type: "image/png")
-wine_tasting.photos.attach(io: file3, filename: "Wine.png", content_type: "image/png")
+wine_tasting.photos.attach([io: file1, filename: "Wine1.png", content_type: "image/png"], [io: file2, filename: "Wine2.png", content_type: "image/png"], [io: file3, filename: "Wine3.png", content_type: "image/png"])
+
 wine_tasting.user = maike
 wine_tasting.save!
+
 
 file1 = URI.open("https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGlraW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60")
 file2 = URI.open("https://images.unsplash.com/photo-1642933196736-1138b99bd5da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80")
 file3 = URI.open("https://images.unsplash.com/photo-1440186347098-386b7459ad6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80")
 hiking = Tour.new(name: "Hiking", date: "28/06/2023", time: "11:00", description: "Come explore and take in breathtaking views, come be one with nature! Come along with your hiking boots, water bottle and hat.", capacity: 4, duration: "120 minutes", start_point: "226, Delftweg, Overschie, Rotterdam")
-hiking.photos.attach(io: file1, filename: "Hiking.png", content_type: "image/png")
-hiking.photos.attach(io: file2, filename: "Hiking.png", content_type: "image/png")
-hiking.photos.attach(io: file3, filename: "Hiking.png", content_type: "image/png")
+hiking.photos.attach([io: file1, filename: "Hiking1.png", content_type: "image/png"], [io: file2, filename: "Hiking2.png", content_type: "image/png"], [io: file3, filename: "Hiking3.png", content_type: "image/png"])
+
 hiking.user = yue
 hiking.save!
 
@@ -64,9 +63,8 @@ file1 = URI.open("https://images.unsplash.com/photo-1516633630673-67bbad747022?i
 file2 = URI.open("https://images.unsplash.com/photo-1493225255756-d9584f8606e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3VyZmluZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60")
 file3 = URI.open("https://images.unsplash.com/photo-1517699418036-fb5d179fef0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1034&q=80")
 surfing = Tour.new(name: "Surfing", date: "28/06/2023", time: "10:00", description: "Massive, big or small waves, ride on!", capacity: 4, duration: "60 minutes", start_point: "Bellariarain 8038 Zürich")
-surfing.photos.attach(io: file1, filename: "Surfing.png", content_type: "image/png")
-surfing.photos.attach(io: file2, filename: "Surfing.png", content_type: "image/png")
-surfing.photos.attach(io: file3, filename: "Surfing.png", content_type: "image/png")
+surfing.photos.attach([io: file1, filename: "Surfing1.png", content_type: "image/png"], [io: file2, filename: "Surfing2.png", content_type: "image/png"], [io: file3, filename: "Surfing3.png", content_type: "image/png"])
+
 surfing.user = jose
 surfing.save!
 
@@ -74,9 +72,8 @@ file1 = URI.open("https://images.unsplash.com/photo-1415125721330-116ba2f81152?i
 file2 = URI.open("https://images.unsplash.com/photo-1566577350641-aeb616b7f477?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80")
 file3 = URI.open("https://images.unsplash.com/photo-1577366773073-a57cdf7132cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGJ1ZGFwZXN0fGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60")
 sight_seeing = Tour.new(name: "Sight-seeing", date: "28/06/2023", time: "13:00", description: "Lets explore my city-centre together, let's visit my city's historic places.", capacity: 5, duration: "120 minutes", start_point: "Hauptstr. 14, 25488 Holm, Schleswig-Holstein Germany")
-sight_seeing.photos.attach(io: file1, filename: "Sighseeing.png", content_type: "image/png")
-sight_seeing.photos.attach(io: file2, filename: "Sighseeing.png", content_type: "image/png")
-sight_seeing.photos.attach(io: file3, filename: "Sighseeing.png", content_type: "image/png")
+sight_seeing.photos.attach([io: file1, filename: "Sightseeing1.png", content_type: "image/png"], [io: file2, filename: "Sightseeing2.png", content_type: "image/png"], [io: file3, filename: "Sightseeing3.png", content_type: "image/png"])
+
 sight_seeing.user = tolu
 sight_seeing.save!
 
@@ -84,8 +81,7 @@ file1 = URI.open("https://images.unsplash.com/photo-1502126324834-38f8e02d7160?i
 file2 = URI.open("https://images.unsplash.com/photo-1519904981063-b0cf448d479e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80")
 file3 = URI.open("https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fG1vdW50YWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60")
 mountain_climbing = Tour.new(name: "Mountain-climbing", date: "01/07/2023", time: "10:00", description: "Are you a sucker for the best views in the world? Get atop a mountain! Come-on, what are you waitng for?", capacity: 5, duration: "120 minutes", start_point: "Nordmarksvej 8, 7190 Billund, Denmark")
-mountain_climbing.photos.attach(io: file1, filename: "Mountainclimb.png", content_type: "image/png")
-mountain_climbing.photos.attach(io: file2, filename: "Mountainclimb.png", content_type: "image/png")
-mountain_climbing.photos.attach(io: file3, filename: "Mountainclimb.png", content_type: "image/png")
+mountain_climbing.photos.attach([io: file1, filename: "Mountainclimb1.png", content_type: "image/png"], [io: file2, filename: "Mountainclimb2.png", content_type: "image/png"], [io: file3, filename: "Mountainclimb3.png", content_type: "image/png"])
+
 mountain_climbing.user = max
 mountain_climbing.save!
