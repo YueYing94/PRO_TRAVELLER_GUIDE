290c72d0caf021d63c934fc6bc64e14c9aae0e33
Jose Torres <jmmot20@gmail.com>
2023-07-01 15:45:39 +0100

    Fixed errors, authorized every user to bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,18 @@
 class BookmarksController < ApplicationController
   def create
     @tour = Tour.find(params[:tour_id])
-    current_user.bookmarked_tours << @tour
+    @bookmark = Bookmark.new
+    @bookmark.tour = @tour
+    @bookmark.user = current_user
+    authorize @bookmark
+    @bookmark.save
     redirect_to profile_path(current_user)
   end
-  resources :tours do
-    post 'bookmark', to: 'bookmarks#create', as: :bookmark
-  end
+
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to profile_path, notice: 'Tour unbookmarked successfully.'
   end
+
 end
